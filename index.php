f3688d30b5f97a645ea6c3ce7a669dfd04e60267
nzeesports <fakefake3179@gmail.com>
2024-12-07 21:34:45 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$get = $_GET['get'];
+$mpdUrl = 'https://linearjitp-playback.astro.com.my/dash-wv/linear/' . $get;
+
+$mpdheads = [
+  'http' => [
+      'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36\r\n",
+      'follow_location' => 1,
+      'timeout' => 5
+  ]
+];
+$context = stream_context_create($mpdheads);
+$res = file_get_contents($mpdUrl, false, $context);
+echo $res;
+?><?php
+$get = $_GET['get'];
+$mpdUrl = 'https://d1yws6emo43ny.cloudfront.net/' . $get;
+
+$mpdheads = [
+  'http' => [
+      'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36\r\n",
+      'follow_location' => 1,
+      'timeout' => 5
+  ]
+];
+$context = stream_context_create($mpdheads);
+$res = file_get_contents($mpdUrl, false, $context);
+echo $res;
+?>
